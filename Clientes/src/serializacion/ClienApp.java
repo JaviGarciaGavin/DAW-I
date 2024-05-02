2a6fb404740b17dad85be8a18be6a8e879fd4947
JaviGarciaGavin <javier.gargav@educa.jcyl.es>
2024-05-02 11:54:12 +0200

    Revert "Quinto Try"
    This reverts commit 5bd9f89d3c44638afa8e1e0503e25b414ae85410.

--- Clientes/src/serializacion/ClienApp.java
+++ Clientes/src/serializacion/ClienApp.java
@@ -14,7 +14,7 @@ public class ClienApp {
 
 	public static void main(String[] args) {
 
-		System.out.println("Quinta Version");
+		System.out.println("Cuarta Version");
 
 		String [] opciones = {"Nuevo cliente", "Listar clientes", "Salir"};		
 		int opcion;
