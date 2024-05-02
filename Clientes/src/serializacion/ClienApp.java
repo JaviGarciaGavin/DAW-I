5bd9f89d3c44638afa8e1e0503e25b414ae85410
JaviGarciaGavin <javier.gargav@educa.jcyl.es>
2024-05-02 11:46:36 +0200

    Quinto Try

--- Clientes/src/serializacion/ClienApp.java
+++ Clientes/src/serializacion/ClienApp.java
@@ -14,7 +14,7 @@ public class ClienApp {
 
 	public static void main(String[] args) {
 
-		System.out.println("Cuarta Version");
+		System.out.println("Quinta Version");
 
 		String [] opciones = {"Nuevo cliente", "Listar clientes", "Salir"};		
 		int opcion;
