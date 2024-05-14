936bc69d255339f13c77f889ffc08e449cc03f42
JaviGarciaGavin <javier.gargav@educa.jcyl.es>
2024-05-14 08:40:47 +0200

    Mas ejercicios de hashmap
    Signed-off-by: JaviGarciaGavin <javier.gargav@educa.jcyl.es>

---
+++ Colecciones/src/ejer6/App.java
@@ -0,0 +1,44 @@
+package ejer6;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+
+public class App {
+public static void main(String[] args) {
+	String seguir = "si", nombre;
+	char sillon;
+	Scanner teclado=new Scanner(System.in);
+	int anyo;
+	boolean insertado;
+	Academico aux=new Academico();
+	Map<Character, Academico> Academia=new HashMap<Character, Academico>();
+
+	while (seguir.toUpperCase().compareTo("SI")==0) {
+		
+		System.out.println("Nombre del academico");
+		nombre=teclado.next();
+		aux.setNombre(nombre);
+		System.out.println("Año de su ingreso");
+		anyo=teclado.nextInt();
+		aux.setaIngreso(anyo);
+	
+		System.out.println("Letra de su sillon");
+		sillon=teclado.next().charAt(0);
+		
+		Academia.put(sillon, aux);
+		
+		insertado=nuevoAcademico(Academia,aux,sillon);
+		
+	System.out.println("Desea continuar?");
+	seguir=teclado.next();
+}
+	
+	System.out.println(Academia);
+}
+private static boolean nuevoAcademico (Map<Character, Academico> academia, Academico aux, char sillon) {
+	boolean confirmado;
+	return false;
+}
+}
