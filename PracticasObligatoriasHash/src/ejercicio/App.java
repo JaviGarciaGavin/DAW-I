a8016f569adb51f97c5e58436bea274ee00cd4da
JaviGarciaGavin <javier.gargav@educa.jcyl.es>
2024-05-16 10:39:51 +0200

    Practica Hashes

--- PracticasObligatoriasHash/src/ejercicio/App.java
+++ PracticasObligatoriasHash/src/ejercicio/App.java
@@ -8,10 +8,10 @@ import dialogos.Dialogos;
 public class App {
 @SuppressWarnings("unchecked")
 public static void main(String[] args) {
-	String seguir = "si", nombre = "";
+	String seguir = "si", nombre = "",letra="";
 	String rellenar = "";
 	char sillon;
-	int anyo;
+	int anyo,opcion = 0;
 	Scanner teclado=new Scanner(System.in);
 	boolean insertado;
 	Academico aux;
@@ -26,7 +26,7 @@ public static void main(String[] args) {
 		anyo = Dialogos.pedirEntero(rellenar);
 	
 		System.out.println("Letra de su sillon");
-		sillon=teclado.next().charAt(0);
+		sillon=Dialogos.pedirCadena(rellenar).charAt(0);
 		
 		aux=new Academico(nombre,anyo);
 		insertado=nuevoAcademico(Academia,aux,sillon);
@@ -35,7 +35,22 @@ public static void main(String[] args) {
 		
 	System.out.println("Desea continuar?");
 	seguir=teclado.next();
+
+	
 }
+	   //Escribir el map de academicos ordenados POR LETRA en un fichero
+    try {
+		FileOutputStream escribir=new FileOutputStream("Academicos.dat");
+		ObjectOutputStream escribirOb= new ObjectOutputStream(escribir);
+
+		escribirOb.writeObject(Academia);
+	}
+	catch (FileNotFoundException e) {}
+	catch (IOException e) {}
+	
+
+	System.out.println("El fichero ha sido creado");
+	
 	
 	 //Listado ordenado por letra
 		Set<Map.Entry<Character, Academico>> conLetra = Academia.entrySet();
@@ -60,8 +75,104 @@ public static void main(String[] args) {
 	     Collections.sort(listaSinLetra,cl2);
 	     System.out.println("\nOrdenado por Ingreso: " + listaSinLetra);	
 	     System.out.println("----------------------------------------------------------------------------------------------------------------------------------");
+
 	
+	  
+	     
+		  //EJERCICIO2
+	     System.out.println("Que desea hacer");
+	    System.out.println("1---->Listar los académicos por orden de nombre");
+	    System.out.println("2---->Listar los académicos por orden de letra");
+	    System.out.println("3---->Añadir un académico");
+	    System.out.println("4---->Borrar un académico pidiendo su letra");
+	    System.out.println("5---->Salir");
+	    
+	    while (opcion !=5) {
+	    opcion=Dialogos.pedirEntero(rellenar);
+	
+		switch (opcion) {
+		case 1: {
+		    //LEER LOS DATOS DEL FICHERO
+			try {
+		 		FileInputStream lector=new FileInputStream("Academicos.dat");
+		 		ObjectInputStream leerOb= new ObjectInputStream(lector);
+		 	
+		 		Academia=(Map<Character, Academico>) leerOb.readObject();
+		 
+		 		System.out.println(Academia);
+		 	
+		 		}	
+		 	
+		 	catch (FileNotFoundException e) {
+		 		e.printStackTrace();
+		 	}
+		 	catch (IOException e) {
+		 		e.printStackTrace();
+		 	} 
+		 	catch (ClassNotFoundException e) {
+		 		e.printStackTrace();
+		 	}
+			break;
+		}
+		case 2: {
+			
+			
+			
+			
+			break;
+		}
+		case 3: {
+		
+			try {
+				FileOutputStream escribir = new FileOutputStream("Academicos.dat");
+				ObjectOutputStream escribirOb= new ObjectOutputStream(escribir);
+				
+				System.out.println("Nombre del academico");
+				nombre=Dialogos.pedirCadena(rellenar);
+				System.out.println("Año de su ingreso");
+				anyo = Dialogos.pedirEntero(rellenar);
+			
+				System.out.println("Letra de su sillon");
+				sillon=Dialogos.pedirCadena(rellenar).charAt(0);
+				
+				aux=new Academico(nombre,anyo);
+				
+				insertado=nuevoAcademico(Academia,aux,sillon);
+				escribirOb.writeObject(Academia);
+				if (insertado==false)
+					System.out.println("No se ha podido insertar. Letra incorrecta");
+			
+			
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			break;
 }
+		case 4: {
+	
+			
+			
+			break;
+}
+		case 5: {
+	
+			
+			
+			break;
+}
+		default:
+			throw new IllegalArgumentException("Unexpected value: ");
+		}
+	    }
+
+
+
+}
+
+
 
 static boolean nuevoAcademico(Map<Character, Academico> academia, Academico nuevo, Character letra) {
     boolean insertado = false;
